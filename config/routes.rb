c0dba2209f95dc9c9bcdc62bb5300f847ea57dac
Rishabh Tayal <rtayal11@gmail.com>
2015-07-20 15:47:58 -0500

    client delete oayment method

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 	post '/nonce/transaction' => 'api#transaction'
 
 	post '/create_customer' => 'api#create_customer'
+	post '/deletePaymentMethod' => 'api#deletePaymentMethod'
+
 	post '/create_merchant' => 'api#create_merchant'
 	post '/find_merchant' => 'api#find_merchant'
 	post '/update_merchant' => 'api#update_merchant'
